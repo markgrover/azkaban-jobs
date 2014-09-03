5153a60b4fef78c46dc0531f741569efa2306238
Mark Grover <mgrover@cloudera.com>
2014-09-03 14:46:36 -0700

    More changes

--- capture-and-decide/process_data_OR_validation_failure.sh
+++ capture-and-decide/process_data_OR_validation_failure.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -x
 if [ -n ${JOB_PROP_FILE} ] && [ -e ${JOB_PROP_FILE} ]; then
-  . ${JOB_{PROP_FILE}
+  #. ${JOB_{PROP_FILE}
   # variable value can also be parsed by the following statement. It returns the value part of validate_erros=value
-  #validate_errors=$(grep -Po "(?<=^validate_errors=).*" $JOB_PROP_FILE)
+  VALIDATE_ERRORS=$(grep -Po "(?<=^validate_errors=).*" $JOB_PROP_FILE)
+  INPUT_BASE_DIR=$(grep -Po "(?<=^input.base.dir=).*" $JOB_PROP_FILE)
+  ERRORS_BASE_DIR=$(grep -Po "(?<=^errors.base.dir=).*" $JOB_PROP_FILE)
 fi
 
-if [ -n "${validate_errors}" ] && [ "${validate_errors}" == "true" ]; then 
+if [ -n "${VALIDATE_ERRORS}" ] && [ "${VALIDATE_ERRORS}" == "true" ]; then 
   echo "Errors found"
-  java  -Xms64M -Xmx256M -Dinput.dir=${input.base.dir}/dataset com.hadooparchitecturebook.MoveOutputToErrorsAction \
-  ${input.base.dir} ${errors.base.dir}
+  java  -Xms64M -Xmx256M com.hadooparchitecturebook.MoveOutputToErrorsAction \
+  ${INPUT_BASE_DIR} ${ERRORS_BASE_DIR}
 else
   echo "No errors found"
-  java -Xms64M -Xmx256M -Dinput.dir=${input.base.dir}/dataset com.hadooparchitecturebook.ProcessDataRunner \
+  java -Xms64M -Xmx256M -Dinput.dir=${INPUT_BASE_DIR}/dataset com.hadooparchitecturebook.ProcessDataRunner \
   wordcount /user/hive/warehouse/table1/data.txt /tmp/wordcount
 fi
---
+++ test1/test1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "File is ${JOB_PROP_FILE}"
+cat ${JOB_PROP_FILE}
