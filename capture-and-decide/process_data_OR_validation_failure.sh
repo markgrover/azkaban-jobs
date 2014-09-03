9a4a1e5295540b725620f9c340526f53bcde7019
Mark Grover <mgrover@cloudera.com>
2014-09-03 14:51:05 -0700

    More checks and balances

--- capture-and-decide/process_data_OR_validation_failure.sh
+++ capture-and-decide/process_data_OR_validation_failure.sh
@@ -1,14 +1,24 @@
 #!/bin/bash -x
+  # ${JOB_PROP_FILE} is always defined and exist so this check should always pass
 if [ -n ${JOB_PROP_FILE} ] && [ -e ${JOB_PROP_FILE} ]; then
-  #. ${JOB_{PROP_FILE}
   # variable value can also be parsed by the following statement. It returns the value part of validate_erros=value
-  VALIDATE_ERRORS=$(grep -Po "(?<=^validate_errors=).*" $JOB_PROP_FILE)
+  #. ${JOB_{PROP_FILE}
   INPUT_BASE_DIR=$(grep -Po "(?<=^input.base.dir=).*" $JOB_PROP_FILE)
+  VALIDATE_ERRORS=$(grep -Po "(?<=^validate_errors=).*" $JOB_PROP_FILE)
   ERRORS_BASE_DIR=$(grep -Po "(?<=^errors.base.dir=).*" $JOB_PROP_FILE)
+fi
+
+if [ -z ${INPUT_BASE_DIR} ]; then
+  echo "No property called input.base.dir defined. Exiting"
+  exit 1
 fi
 
 if [ -n "${VALIDATE_ERRORS}" ] && [ "${VALIDATE_ERRORS}" == "true" ]; then 
   echo "Errors found"
+  if [ -z ${ERRORS_BASE_DIR} ]; then
+    echo "No property called errors.base.dir defined. Exiting"
+    exit 1
+  fi
   java  -Xms64M -Xmx256M com.hadooparchitecturebook.MoveOutputToErrorsAction \
   ${INPUT_BASE_DIR} ${ERRORS_BASE_DIR}
 else
